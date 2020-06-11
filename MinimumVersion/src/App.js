7fdd8d566fcb9aa366bdeb6a9f50e1d1612be8c7
bohan0lin <63618439+bohan0lin@users.noreply.github.com>
2020-06-11 22:42:27 +0800

    Add home.js

--- MinimumVersion/src/App.js
+++ MinimumVersion/src/App.js
@@ -6,20 +6,19 @@ import CardList from './containers/CardList';
 import saleData from '../assets/sales.js';
 import Header from './components/Header/Header';
 import Account from './components/Account/Account';
+import Home from './containers/Home';
 
 export default function App()  {
   return (
    <Router>
     <Header />
 
-    <div className='container-fluid'>
-      <nav className='navbar sticky-top navbar-light bg-white'>
-        <h1 className='navbar-brand text-dark'>Newly Listed</h1>
-      </nav>
-      <CardList data={saleData} />
-    </div>
+
 
     <Switch>
+      <Route  exact path = "/">
+        <Home />
+      </Route>
       <Route  path = "/Account">
         <Account />
       </Route>
---
+++ MinimumVersion/src/containers/Home.js
@@ -0,0 +1,20 @@
+import React from'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import CardList from './CardList';
+import saleData from '../../assets/sales.js';
+
+
+export default function Home()  {
+  return (
+
+    <div className='container-fluid'>
+      <nav className='navbar sticky-top navbar-light bg-white'>
+        <h1 className='navbar-brand text-dark'>Newly Listed</h1>
+      </nav>
+      <CardList data={saleData} />
+    </div>
+
+  );
+}
