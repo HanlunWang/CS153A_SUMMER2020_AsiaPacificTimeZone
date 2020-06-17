0565a19343d07a39ccd88dd4a17d8254b9bae4f4
bohan0lin <63618439+bohan0lin@users.noreply.github.com>
2020-06-17 19:12:08 +0800

    creat react native project

---
+++ ReactNativeProject/Components/Screens/AddItem.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function AddItem() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Add an item for sale!</Text>
+    </View>
+  );
+}
---
+++ ReactNativeProject/Components/Screens/Cart.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Cart() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>This is my shopping cart!</Text>
+    </View>
+  );
+}
